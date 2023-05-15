889abf9ee1190b8a1e3c74e1ea497037decf4ea0
@syabdulr <abdulsyed1998@gmail.com>
2023-05-15 16:45:32 -0400

    add pages that display urls that have shortened http link

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8081; // default port 8080
+const PORT = 8081; // default port 8081
 
 app.set("view engine", "ejs");
 
@@ -17,10 +17,14 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }; // Pass urlDatabase as a variable to the template
+  res.render("urls_index", templateVars); // Render the "urls_index.ejs" template with the provided data
 });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = { id, longURL };
+  res.render("urls_show", templateVars);
+});
