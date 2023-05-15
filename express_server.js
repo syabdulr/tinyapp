69a87a72a0e2c70bdfcdc8a1bae913e9b9647174
@syabdulr <abdulsyed1998@gmail.com>
2023-05-15 18:02:50 -0400

    get url from user input and post it as a console.log for the current iteration

--- express_server.js
+++ express_server.js
@@ -2,24 +2,47 @@ const express = require("express");
 const app = express();
 const PORT = 8081; // default port 8081
 
+// link to website http://localhost:8081/urls
+
 app.set("view engine", "ejs");
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; // Pass urlDatabase as a variable to the template
   res.render("urls_index", templateVars); // Render the "urls_index.ejs" template with the provided data
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -27,4 +50,8 @@ app.get("/urls/:id", (req, res) => {
   const longURL = urlDatabase[id];
   const templateVars = { id, longURL };
   res.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
