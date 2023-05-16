24d8687f41cd893fa994edfa3a02d6db1db0d637
@syabdulr <abdulsyed1998@gmail.com>
2023-05-16 14:19:08 -0400

    add nodemon so that the changes to source code will now automatically restart the server

--- express_server.js
+++ express_server.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8081; // default port 8081
 
-// link to website http://localhost:8081/urls
-
+// Set the view engine to EJS
 app.set("view engine", "ejs");
 
-
-
+// Database storing the URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Middleware to parse the request body
 app.use(express.urlencoded({ extended: true }));
 
+// Function to generate a random string
 function generateRandomString() {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -25,42 +25,46 @@ function generateRandomString() {
   return result;
 }
 
-
+// Route handler for the root path
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
+// Route handler to render the URLs index page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }; // Pass urlDatabase as a variable to the template
+  const templateVars = { urls: urlDatabase }; // Provide the URL database to the template
   res.render("urls_index", templateVars); // Render the "urls_index.ejs" template with the provided data
 });
 
-// this route handler handles form submission (post request)
+// Route handler for form submission to create a new URL
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  const longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls/${shortURL}`); // in the http line, new url
+  const shortURL = generateRandomString(); // Generate a random short URL
+  const longURL = req.body.longURL; // Get the long URL from the form submission
+  urlDatabase[shortURL] = longURL; // Store the long URL in the database with the short URL as the key
+  res.redirect(`/urls/${shortURL}`); // Redirect to the page for the newly created short URL
 });
 
+// Route handler to render the new URL submission form
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new"); // Render the "urls_new.ejs" template
 });
 
+// Route handler to render the page for a specific URL
 app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  const templateVars = { id, longURL };
-  res.render("urls_show", templateVars);
+  const id = req.params.id; // Get the short URL from the route parameter
+  const longURL = urlDatabase[id]; // Look up the corresponding long URL in the database
+  const templateVars = { id, longURL }; // Provide the short URL and long URL to the template
+  res.render("urls_show", templateVars); // Render the "urls_show.ejs" template with the provided data
 });
 
+// Route handler to redirect to the longURL associated with the shortURL
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id; // Get the short URL from the route parameter
+  const longURL = urlDatabase[shortURL]; // Look up the corresponding long URL in the database
+  res.redirect(longURL); // Redirect the user to the long URL
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/u/:id", (req, res) => {
-  const shortURL = req.params.id;
-  const longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
 });
