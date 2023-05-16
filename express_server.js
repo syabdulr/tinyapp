3e0b281b7fd88a949e8be0dbeeb8c0afb7f75a60
@syabdulr <abdulsyed1998@gmail.com>
2023-05-16 17:42:06 -0400

    create a form that takes uername and stores that value in cookies while navigating through other pages

--- express_server.js
+++ express_server.js
@@ -77,6 +77,12 @@ app.post("/urls/:id/delete", (req,res) => {
   res.redirect('/urls');
 });
 
+app.post("/login", (req,res)=>{
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+  //console.log(username);test to store username
+});
 
 // Start the server
 app.listen(PORT, () => {
