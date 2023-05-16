3d1d068c49a9faddaccb6b80cd5a4533f24268af
@syabdulr <abdulsyed1998@gmail.com>
2023-05-16 18:54:08 -0400

    display form based on whether or not user has logged in. If logged in display logout form. If not logged in, display login form. Also added comments for clarity

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+var cookieParser = require('cookie-parser'); //needed for username
 const app = express();
 const PORT = 8081; // default port 8081
+app.use(cookieParser());
 
 // Set the view engine to EJS
 app.set("view engine", "ejs");
@@ -32,13 +34,17 @@ app.get("/", (req, res) => {
 
 // Route handler to render the URLs index page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }; // Provide the URL database to the template
+  const username = req.cookies.username;
+
+  const templateVars = { 
+    urls: urlDatabase, 
+    username, // Add the username to the data passed to the view
+  }; // Provide the URL database to the template
   res.render("urls_index", templateVars); // Render the "urls_index.ejs" template with the provided data
 });
 
 // Route handler for form submission to create a new URL
 app.post("/urls", (req, res) => {
-  console.log("inside creation");
   const shortURL = generateRandomString(); // Generate a random short URL
   const longURL = req.body.longURL; // Get the long URL from the form submission
   urlDatabase[shortURL] = longURL; // Store the long URL in the database with the short URL as the key
@@ -47,14 +53,16 @@ app.post("/urls", (req, res) => {
 
 // Route handler to render the new URL submission form
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new"); // Render the "urls_new.ejs" template
+  const username = req.cookies.username;
+  res.render("urls_new",{username}); // Render the "urls_new.ejs" template
 });
 
 // Route handler to render the page for a specific URL
 app.get("/urls/:id", (req, res) => {
+  const username = req.cookies.username;
   const id = req.params.id; // Get the short URL from the route parameter
   const longURL = urlDatabase[id]; // Look up the corresponding long URL in the database
-  const templateVars = { id, longURL }; // Provide the short URL and long URL to the template
+  const templateVars = { id, longURL, username }; // Provide the short URL and long URL to the template
   res.render("urls_show", templateVars); // Render the "urls_show.ejs" template with the provided data
 });
 
@@ -64,27 +72,38 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[shortURL]; // Look up the corresponding long URL in the database
   res.redirect(longURL); // Redirect the user to the long URL
 });
-
-app.post("/urls/:id", (req,res) => { //edit 
-  const id = req.params.id;
-  urlDatabase[id] = req.body.longURL;
-  res.redirect('/urls'); 
+// This route handles the request to edit a URL. It gets the id from the request parameters,
+// sets the new long URL from the request body, and then redirects the client to the URLs page.
+app.post("/urls/:id", (req, res) => { 
+  const id = req.params.id;  // Get the id from the request parameters
+  urlDatabase[id] = req.body.longURL;  // Set the new long URL from the request body
+  res.redirect('/urls');  // Redirect the client to the URLs page
 });
 
-app.post("/urls/:id/delete", (req,res) => {
-  const id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect('/urls');
+// This route handles the request to delete a URL. It gets the id from the request parameters,
+// deletes the corresponding entry in the URL database, and then redirects the client to the URLs page.
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;  // Get the id from the request parameters
+  delete urlDatabase[id];  // Delete the corresponding entry in the URL database
+  res.redirect('/urls');  // Redirect the client to the URLs page
 });
 
-app.post("/login", (req,res)=>{
-  const username = req.body.username;
-  res.cookie('username', username);
-  res.redirect('/urls');
-  //console.log(username);test to store username
+// This route handles the request to log in. It gets the username from the request body,
+// sets a cookie with the username, and then redirects the client to the URLs page.
+app.post("/login", (req, res) => {
+  const username = req.body.username;  // Get the username from the request body
+  res.cookie('username', username);  // Set a cookie with the username
+  res.redirect('/urls');  // Redirect the client to the URLs page
 });
 
-// Start the server
+// This route handles the request to log out. It clears the username cookie, 
+// and then redirects the client to the URLs page.
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');  // Clear the username cookie
+  res.redirect('/urls');  // Redirect the client to the URLs page
+});
+
+// Start the server and listen for connections on the specified port.
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);  // Log a message to the console when the server starts
 });
